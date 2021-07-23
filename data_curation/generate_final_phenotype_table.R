e579cbb42f8215a79baf27012701410156969039
tayabsoomro <isoomro.t@gmail.com>
2021-07-23 12:14:07 -0600

    generate the first PCA biplot

---
+++ data_curation/generate_final_phenotype_table.R
@@ -0,0 +1,115 @@
+# Title     : Generate Final Apples Table
+# Objective : This script generates a list file which contains the phenotype
+#             data for all the apples that are used in our analyses
+
+library(ggplot2)
+
+###############################
+## CIDER APPLES LIST CLEANUP ##
+###############################
+
+# get the final cider apple list
+final_cider.df <- read.table(
+  'data/processed/final_cider_apple_phenotype_data.tsv',
+  header = TRUE
+)
+
+# only keep English and French cider apples
+final_cider.df <- 
+  final_cider.df[
+    which(final_cider.df$Region.of.origin == "England" | final_cider.df$Region.of.origin == "France"),
+  ]
+
+# only keep certain columns
+final_cider.df <- final_cider.df[,c(
+  "Accession.name",
+  "acidity_17_harv",
+  "percent_acidity_17",
+  "brix_17_harv",
+  "firmness_avg_17_harv",
+  "weight_avg_17_harv",
+  "juiciness_16_harv",
+  "tpc",
+  "date_jul_16_harv",
+  "flowering_jul_16_harv",
+  "percent_firmness_avg_17",
+  "Region.of.origin"
+)]
+
+# cleanup the column names
+colnames(final_cider.df) <- c(
+  "Name",
+  "Acidity",
+  "DeltaAcidity", # change in acidity during storage
+  "SSC",
+  "Firmness",
+  "Weight",
+  "Juiciness",
+  "PhenolicContent",
+  "HarvestDate",
+  "FloweringDate",
+  "Softening", # % Change in firmness during storage,
+  "AppleType"
+)
+
+nrow(final_cider.df)
+# [1] 83
+
+#################################
+## DESSERT APPLES LIST CLEANUP ##
+#################################
+
+# get the final dessert apple list
+final_dessert.df <- read.table(
+  'data/processed/final_dessert_apple_phenotype_data.tsv',
+  header = TRUE
+)
+
+# only keep certain columns
+final_dessert.df <- final_dessert.df[,c(
+  "PLANTID",
+  "acidity_17_harv",
+  "percent_acidity_17",
+  "brix_17_harv",
+  "firmness_avg_17_harv",
+  "weight_avg_17_harv",
+  "juiciness_16_harv",
+  "tpc",
+  "date_jul_16_harv",
+  "flowering_jul_16_harv",
+  "percent_firmness_avg_17"
+)]
+
+# cleanup the column names
+colnames(final_dessert.df) <- c(
+  "Name",
+  "Acidity",
+  "DeltaAcidity", # change in acidity during storage
+  "SSC",
+  "Firmness",
+  "Weight",
+  "Juiciness",
+  "PhenolicContent",
+  "HarvestDate",
+  "FloweringDate",
+  "Softening" # % Change in firmness during storage,
+)
+
+# add AppleType column
+final_dessert.df$AppleType = "Dessert"
+
+nrow(final_dessert.df)
+# [1] 16
+
+######################
+## COMBINED DATASET ##
+######################
+
+final.df <- rbind(final_cider.df, final_dessert.df)
+
+# save the final table
+write.table(
+  final.df,
+  'data/processed/final_phenotype_table.tsv',
+  row.names = FALSE
+)
