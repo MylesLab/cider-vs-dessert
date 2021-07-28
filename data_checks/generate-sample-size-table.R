129f5be4625083c364a92ba0efed632803769da1
tayabsoomro <isoomro.t@gmail.com>
2021-07-27 21:11:03 -0600

    add code for generating sample sizes table

---
+++ data_checks/generate-sample-size-table.R
@@ -0,0 +1,43 @@
+# Title     : Generate Sample Sizes Table
+# Objective : This script performs some data checks to get some basic statistics
+#             on the final phenotype table
+# Created by: tayabsoomro
+# Created on: 2021-05-31
+
+############################
+## IMPORTS & DATA LOADING ##
+############################
+
+library(dplyr)
+library(readxl)
+library(xlsx)
+
+# load the data
+final.df <- read.table(
+  'data/processed/final_phenotype_table.tsv',
+  header = TRUE
+)
+
+all_phenotype_names <- head(colnames(final.df)[-1],n=-1)
+
+sample_size_table = as.data.frame(matrix(NA, nrow=10,ncol=3))
+colnames(sample_size_table) <- c("Dessert", "English", "French")
+rownames(sample_size_table) <- all_phenotype_names
+
+for(i in seq_along(all_phenotype_names)){
+  phenotype <- colnames(final.df)[i+1]
+  
+  ph_dat <- cbind(
+    sum(!is.na(final.df[which(final.df$AppleType == 'Dessert'),phenotype])),
+    sum(!is.na(final.df[which(final.df$AppleType == 'England'),phenotype])),
+    sum(!is.na(final.df[which(final.df$AppleType == 'France'),phenotype]))
+  )
+  
+  sample_size_table[i,] <- ph_dat
+}
+
+# write sample sizes table
+write.xlsx(
+  sample_size_table,
+  'data/processed/tbl-sample-sizes.xlsx'
+)
