0e757f5b81380037f7ddd3c4b90c27e75d9f0ac7
tayabsoomro <isoomro.t@gmail.com>
2021-08-08 19:31:45 -0600

    add missing data anlaysis

--- data_checks/missing_data_check.R
+++ data_checks/missing_data_check.R
@@ -11,6 +11,9 @@
 library(ggplot2)
 library(ggpubr)
 library(reshape2)
+library(RColorBrewer)
+
+source('themes/theme_avenir.R')
 
 final.df <- read.table(
   'data/processed/final_phenotype_table.tsv',
@@ -31,14 +34,13 @@ bar_dat <- melt(data.frame(
 # generate the bar plot
 data_distribution_barplot <- ggplot(bar_dat, aes(x=variable, y=value, fill=variable)) + 
   geom_text(aes(label=value), vjust=-0.2) + 
-  geom_bar(stat = "identity") + 
-  theme_classic() + 
-  ggtitle("Distribution of apples within our dataset") + 
+  geom_bar(stat = "identity", alpha = 0.8) + 
+  theme_avenir() + 
   theme(
     plot.title = element_text(
       face = "bold", 
       size=18, 
-      hjust=0.5,
+      hjust=0.5
     )
   ) + 
   xlab("Apple Types") + 
@@ -53,49 +55,39 @@ ggsave(
 ## MISSINGNESS BY PHENOTYPES ##
 ###############################
 
-phenotype_missingness.df <- as.data.frame(matrix(nrow=0,ncol=2))
+phenotype_missingness.df <- NULL
 for(p_i in 3:ncol(final.df)-1){
   
   # get the phenotype name
   phenotype <- colnames(final.df)[p_i]
   
-  # get the column as vector
-  pheno_missingness <- round((sum(is.na(final.df[,p_i])) / nrow(final.df)) * 100,2)
+  # the current data frame
+  curr_df <- as.data.frame(
+    table(final.df[which(!is.na(final.df[,phenotype])),'AppleType'])
+  )
+  curr_df$Phenotype <- phenotype
   
-  # add the data
-  phenotype_missingness.df <- 
-    rbind(
-      phenotype_missingness.df,
-      c(phenotype, pheno_missingness)
-    )
+  phenotype_missingness.df <-  rbind(phenotype_missingness.df, curr_df)
+  
 }
 
 # cleaning up the data frame for plotting
-colnames(phenotype_missingness.df) <- c("Phenotype","PercentMissingness")
-phenotype_missingness.df$PercentMissingness <-  as.numeric(phenotype_missingness.df$PercentMissingness)
+colnames(phenotype_missingness.df) <- c("AppleType", "Count", "Phenotype")
 
-# plot the missing ness distribution
 missingness_by_phenotypes <- ggplot(
-  phenotype_missingness.df, 
-  aes(fill=Phenotype, y=reorder(Phenotype, -PercentMissingness), x=PercentMissingness)) + 
-  geom_bar(position="dodge", stat="identity", alpha=0.8) +
-  geom_vline(xintercept = 50, size = 1, color = "red") + 
-  geom_text(aes(label=paste0(PercentMissingness, "%"),x=0), hjust = -0.5) + 
-  theme_classic() + 
-  ggtitle("Distribution of missing data for phenotypes") + 
+  phenotype_missingness.df, aes(fill=AppleType, y=reorder(Phenotype, -Count), x=Count)
+) + 
+  geom_bar(position="stack", stat="identity", alpha = 0.9) + 
+  theme_avenir(grid =  F, panel_x = F, panel_y = F) + 
+  scale_fill_brewer(palette = "Pastel1") + 
   theme(
-   axis.line = element_line(colour = "black"),
-    plot.title = element_text(hjust = 0.5, size=18, face="bold"),
-    axis.title.x=element_text(size=14,face="bold",hjust=0.5),
-    axis.title.y=element_text(size=14,face="bold",hjust=0.5)
-  ) + 
-  scale_fill_manual(values=c(
-    '#8ecae6','#219ebc','#023047','#ffb703','#fb8500','#e76f51','#ccd5ae',
-    '#a5a58d','#bb3e03','#f28482'
-  )) +
-  ylab("Phenotypes") + 
-  xlab("% Missing Samples")
-
+    axis.text.y = element_text(size = 10), 
+    axis.text.x = element_text(size = 10),
+    axis.title.x = element_text(size = 12),
+    axis.title.y = element_text(size = 12),
+    axis.line.x = element_line(colour="black", size = 0.2),
+    axis.line.y = element_line(colour="black", size = 0.2)
+  ) + ylab("")
 ggsave(
   filename = "figures/missing_data/data_missingness_by_phenotypes.png",
   plot = missingness_by_phenotypes
