0baff46660e4ab83b9b3aa24cf70af4c25da96f6
tayabsoomro <isoomro.t@gmail.com>
2022-02-01 21:04:43 -0400

    finalize the final phenotype table

--- data_curation/generate_final_phenotype_table.R
+++ data_curation/generate_final_phenotype_table.R
@@ -21,6 +21,19 @@ NEW_COLNAMES <- c(
   "Softening" # % Change in firmness during storage
 )
 
+phenotype_cols <- c(
+  "acidity_17_harv",
+  "percent_acidity_17",
+  "brix_17_harv",
+  "firmness_avg_17_harv",
+  "weight_avg_17_harv",
+  "juiciness_16_harv",
+  "tpc",
+  "date_jul_17_harv",
+  "flowering_jul_16_harv",
+  "percent_firmness_avg_17"
+)
+
 # get the final cider apple list
 final_cider.df <- read.table(
   'data/processed/final_cider_apple_phenotype_data.tsv',
@@ -33,24 +46,15 @@ dim(final_cider.df)
 final_cider.df <- final_cider.df[, c(
   "PI..no..",
   "Accession.name",
-  "acidity_17_harv",
-  "percent_acidity_17",
-  "brix_17_harv",
-  "firmness_avg_17_harv",
-  "weight_avg_17_harv",
-  "juiciness_16_harv",
-  "tpc",
-  "date_jul_17_harv",
-  "flowering_jul_16_harv",
-  "percent_firmness_avg_17",
+  phenotype_cols,
   "Region.of.origin"
 )]
 
 # cleanup the column names
 colnames(final_cider.df) <- c(NEW_COLNAMES, "AppleType")
 
-nrow(final_cider.df)
-# [1] 83
+dim(final_cider.df)
+# [1] 83 13
 
 #################################
 ## DESSERT APPLES LIST CLEANUP ##
@@ -61,21 +65,14 @@ final_dessert.df <- read.table(
   'data/processed/final_dessert_apple_phenotype_data.tsv',
   header = TRUE
 )
+dim(final_dessert.df)
+# [1] 16 49
 
 # only keep certain columns
 final_dessert.df <- final_dessert.df[,c(
   "ACNO",
   "PLANTID",
-  "acidity_17_harv",
-  "percent_acidity_17",
-  "brix_17_harv",
-  "firmness_avg_17_harv",
-  "weight_avg_17_harv",
-  "juiciness_16_harv",
-  "tpc",
-  "date_jul_16_harv",
-  "flowering_jul_16_harv",
-  "percent_firmness_avg_17"
+  phenotype_cols
 )]
 
 # cleanup the column names
@@ -84,8 +81,8 @@ colnames(final_dessert.df) <- NEW_COLNAMES
 # add AppleType column
 final_dessert.df$AppleType <- "Dessert"
 
-nrow(final_dessert.df)
-# [1] 16
+dim(final_dessert.df)
+# [1] 16 13
 
 ######################
 ## COMBINED DATASET ##
@@ -95,19 +92,20 @@ final.df <- rbind(final_cider.df, final_dessert.df)
 dim(final.df)
 # [1] 99 13
 
-########################
-## SAMPLE FILTERATION ##
-########################
+#######################
+## SAMPLE FILTRATION ##
+#######################
 
-# get the rows (accessions) with 50% or less missingness
-phenos <- 3:(ncol(final.df) - 1)
-only_phenos_final.df <- final.df[, phenos]
-filtered_accessions <- which(rowSums(is.na(only_phenos_final.df)) <= 5)
+# get the rows (accessions) with 50% or less missingness in terms of the phenotypes
+phenos_idx <- 3:(ncol(final.df) - 1)
+only_phenos_final.df <- final.df[, phenos_idx]
+filtered_accessions <- which(rowSums(is.na(only_phenos_final.df)) <= 5) # 50% of 10 phenotypes (i.e., 5 phenotypes)
+# should be present for the particular sample to be added
 
 # filter the final dataframe
 final.df <- final.df[filtered_accessions,]
 dim(final.df)
-# [1] 54 13
+# [1] 55 13
 
 ##################
 ## UPDATE NAMES ##
