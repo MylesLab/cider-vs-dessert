227b8e1e3d8d3f43852b5f611775d593e7be654f
tayabsoomro <isoomro.t@gmail.com>
2022-02-04 18:45:35 -0400

    add constants file and add df sizes in comments

---
+++ data_curation/constants.R
@@ -0,0 +1,15 @@
+# Description: This file contains constants that are important for data_curation
+#              module.
+
+phenotype_cols <- c(
+  "acidity_17_harv",
+  "percent_acidity_17",
+  "brix_17_harv",
+  "firmness_avg_17_harv",
+  "weight_avg_17_harv",
+  "juiciness_16_harv",
+  "tpc",
+  "date_jul_17_harv",
+  "flowering_jul_16_harv",
+  "percent_firmness_avg_17"
+)
